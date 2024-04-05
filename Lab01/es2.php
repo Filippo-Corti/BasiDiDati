3fd79ae386fca121dce94fb79efe743b053d5bf3
Filippo-0 <corti.filippo03@gmail.com>
2024-04-05 11:57:44 +0200

    Lab01 finito

--- Lab01/es2.php
+++ Lab01/es2.php
@@ -9,11 +9,16 @@
 </head>
 <body>
 	<style>
+	* {
+		box-sizing: border-box;
+	}
+
 	table, td, tr {
 		border: 1px solid black;
 		border-collapse: collapse;
 		padding: 3px 5px;
 		text-align: left;
+		
 	}
 	</style>
 
@@ -35,17 +40,11 @@
 				<th>Data nascita:</th>
 				<td>
 					<label for="day"> giorno: </label>
-					<select name="day" id="day">
-						<option value="1">1</option>
-					</select>
+					<?php echo selectRange(1, 31, 'day') ?>
 					<label for="month"> mese: </label>
-					<select name="month" id="month">
-						<option value="1">1</option>
-					</select>
+					<?php echo selectRange(1, 12, 'month') ?>
 					<label for="year"> anno: </label>
-					<select name="year" id="year">
-						<option value="1970">1970</option>
-					</select>
+					<?php echo selectRange(1970, 2024, 'year') ?>
 				</td>
 			</tr>
 			<tr>
@@ -57,8 +56,52 @@
 					<label for="female">Femmina </label>
 				</td>
 			</tr>
+			<tr>
+				<th>Attività:</th>
+				<td>
+					<input type="checkbox" id="ski" name="ski" value="Sci">
+					<label for="ski">Sci</label> <br>
+					<input type="checkbox" id="tennis" name="tennis" value="Tennis">
+					<label for="tennis">Tennis</label> <br>
+					<input type="checkbox" id="golf" name="golf" value="Golf">
+					<label for="golf">Golf</label> <br>
+					<input type="checkbox" id="canoe" name="canoe" value="Canoa">
+					<label for="canoe">Canoa</label> <br>
+					<input type="checkbox" id="other" name="other" value="Altro">
+					<label for="other">Altro</label> <br>
+				</td>
+			</tr>
+			<tr>
+				<th>Condizioni di utilizzo</th>
+				<td>
+					<input style="width:100%" type="text" value="bla bla bla bla bla bla bla bla bla bla bla" disabled>
+					<br>
+					<input type="checkbox" id="other" name="other" value="Altro">
+					<label for="other">Altro</label> <br>
+				</td>
+			</tr>
+			<tr>
+				<td style="text-align:center" colspan="2">
+					<input type="submit" value="OK">
+					<button>Cancella</button>
+				</td>
+			</tr>
 		</table>
 	</form>
 
 </body>
+
+<?php
+
+function selectRange($start, $end, $name) {
+	$str = "<select name=$name id=$name>";
+	for ($i = $start; $i <= $end; $i++) {
+		$str .= "<option value=$i>$i</option>";
+	}
+	return $str . "</select>";
+}
+
+?>
+
 </html>
+
